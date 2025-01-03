6a0babc1ffb9c435229b80a7f5f5b93186738eee
Sodam <sodamsummer@gmail.com>
2025-01-03 17:10:27 +0900

    Update

---
+++ contents/community/script/ask.js
@@ -0,0 +1,22 @@
+// ask.js
+
+$(document).ready(function () {
+    const text = "공사중입니다.";
+    let index = 0;
+
+    function typeText() {
+        if (index < text.length) {
+            document.getElementById('typing-container').textContent += text[index];
+            index++;
+            setTimeout(typeText, 150); // 타이핑 속도 설정 (150ms)
+        } else {
+            setTimeout(() => {
+                document.getElementById('typing-container').textContent = '';
+                index = 0;
+                typeText(); // 반복 시작
+            }, 3000); // 3초 대기 후 반복
+        }
+    }
+
+    typeText(); // 첫 타이핑 시작
+});
